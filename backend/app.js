95ce6b6f0e986987d87a8c787c163b9574459512
shashank27 <mr.shashankkumar.alld57@gmail.com>
2023-04-22 15:43:26 +0530

    initial commit

---
+++ backend/app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload");
+const dotenv = require("dotenv");
+const app = express();
+const path = require("path");
+
+const ErrorMiddleware = require("./middleware/error");
+
+//  Config
+dotenv.config({ path: "backend/config/config.env" });
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(fileUpload());
+// Route Imports
+
+const product = require("./routes/productRoute");
+const user = require("./routes/userRoute");
+const order = require("./routes/orderRoute");
+const payment = require("./routes/paymentRoute");
+
+app.use("/api/v1", product);
+app.use("/api/v1", user);
+app.use("/api/v1", order);
+app.use("/api/v1", payment);
+
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/index.html"));
+});
+
+// Middleware for Error
+app.use(ErrorMiddleware);
+
+module.exports = app;
